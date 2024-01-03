e9ebd9d4f3c6d7246952fa734f265c22495b703c
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-01-03 21:30:10 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,34 @@ import Footer from './component/Layout/Footer';
 import './App.css';
 import Home from './component/pages/Home';
 import About from './component/pages/About';
+import Alert from './component/pages/Alert';
+import Product from './component/pages/product';
 import Notfound from './component/pages/Notfound';
 import { GithubProvider } from './context/Github/GitHubContext';
+import { Alertprovider } from './context/Alert/Alertcontext';
 
 const App = () => {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <Alertprovider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main className="container mx-auto px-3 pb-12 text-white">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<Notfound />} />
-              <Route path="/*" element={<Notfound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+            <main className="container mx-auto px-3 pb-12 text-white">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/product/:login" element={<Product />} />
+                <Route path="/notfound" element={<Notfound />} />
+                <Route path="/*" element={<Notfound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </Alertprovider>
     </GithubProvider>
   );
 };
